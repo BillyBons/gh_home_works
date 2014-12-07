c42b18b0759e31a6e4d659b8b0ba5795750d5d89
Billy <admin@iMac-admin.local>
2014-12-07 22:01:24 +0200

    HomeTask8

---
+++ HomeTask8/HomeTask8/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HomeTask8
+//
+//  Created by Billy on 06.12.14.
+//  Copyright (c) 2014 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
