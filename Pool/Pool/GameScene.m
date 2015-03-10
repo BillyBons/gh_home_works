5a3bb3fc4e04ee3d40e26dd5bcaa5bec3bb8f8b7
Billy <admin@iMac.local>
2015-03-10 23:49:27 +0200

    last pool

--- Pool/Pool/GameScene.m
+++ Pool/Pool/GameScene.m
@@ -8,20 +8,32 @@
 
 #import "GameScene.h"
 
+static const uint32_t ballCategory = 0x1 << 1;
+
+static const uint32_t cueCategory = 0x1 << 1;
+
 @implementation GameScene
 
 -(void)didMoveToView:(SKView *)view {
     /* Setup your scene here */
-    [self initWithSize:CGSizeMake(568, 320)];
+    self.physicsWorld.contactDelegate = self;
+    [self initWithSize:CGSizeMake(540, 320)];
+    
     [self BouncingBall];
-    self.physicsWorld.gravity = CGVectorMake(0.0f, 0.0f);
+    
+}
+
+- (void)didBeginContact:(SKPhysicsContact *)contact
+{
+    NSLog(@"BOOM");
 }
 
 -(id)initWithSize:(CGSize)size {
     if (self = [super initWithSize:size]) {
-//        SKSpriteNode* background = [SKSpriteNode spriteNodeWithImageNamed:@"bg.png"];
-//        background.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
-//        [self addChild:background];
+        SKSpriteNode* background = [SKSpriteNode spriteNodeWithImageNamed:@"bg.png"];
+        background.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
+        [self addChild:background];
+        self.physicsWorld.gravity = CGVectorMake(0.0f, 0.0f);
     }
     return self;
 }
@@ -30,30 +42,34 @@
     /* Called when a touch begins */
     UITouch* touch = [touches anyObject];
     CGPoint touchLocation = [touch locationInNode:self];
-    
+    //NSLog(@"touch x = %f,  touch y = %f,",touchLocation.x, touchLocation.y);
     SKPhysicsBody* body = [self.physicsWorld bodyAtPoint:touchLocation];
     if (body && [body.node.name isEqualToString: @"Cue"]) {
-        NSLog(@"Began touch on paddle");
+        NSLog(@"Began touch on cue");
         self.isFingerOnCue = YES;
+        
     }
 }
 
 -(void)touchesMoved:(NSSet*)touches withEvent:(UIEvent*)event{
-    // 1 Check whether user tapped paddle
+    // 1 Check whether user tapped cue
     if (self.isFingerOnCue) {
         // 2 Get touch location
+        
         UITouch* touch = [touches anyObject];
         CGPoint touchLocation = [touch locationInNode:self];
         CGPoint previousLocation = [touch previousLocationInNode:self];
         // 3 Get node for cue
         SKShapeNode* cue = (SKShapeNode*)[self childNodeWithName: @"Cue"];
+       
         // 4 Calculate new position along x for cue
         int cueX = cue.position.x + (touchLocation.x - previousLocation.x);
         int cueY = cue.position.y + (previousLocation.y - touchLocation.y);
+        //NSLog(@"Cue x = %d,  Cue y = %d,",cueX, cueY);
         // 5 Limit x so that the cue will not leave the screen to left or right
         //cueX = MAX(cueX, cue.size.width/2);
         //cueX = MIN(cueX, self.size.width - cue.size.width/2);
-        // 6 Update position of paddle
+        // 6 Update position of cue
         cue.position = CGPointMake(cueX, cueY);
     }}
 
@@ -71,36 +87,47 @@
     
     SKSpriteNode* ball = [SKSpriteNode spriteNodeWithImageNamed: @"ball_white.png"];
     ball.name = @"ball";
-    ball.position = CGPointMake(self.frame.size.width/5, self.frame.size.height/2);
-    [self addChild:ball];
+    ball.position = CGPointMake(self.frame.size.width/3, self.frame.size.height/2);
     
-    ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    SKPhysicsBody *ball1 = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+    [ball setPhysicsBody:ball1];
+    
     ball.physicsBody.friction = 0.1f;
     ball.physicsBody.restitution = 1.0f;
     ball.physicsBody.linearDamping = 0.1f;
     ball.physicsBody.allowsRotation = NO;
     
     
-    [ball.physicsBody applyImpulse:CGVectorMake(10.0f, 1.5f)];
+    ball1.categoryBitMask = ballCategory;
+    ball1.collisionBitMask = cueCategory;
+    ball1.contactTestBitMask = cueCategory;
+    
+    [self addChild:ball];
+    //ball.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball.frame.size.width/2];
+   
+    
+    
+    
+    
+    [ball.physicsBody applyImpulse:CGVectorMake(10.0f, 0.3f)];
     
     
     SKSpriteNode* ball2 = [SKSpriteNode spriteNodeWithImageNamed: @"ball_green.png"];
     ball2.name = @"ball2";
     ball2.position = CGPointMake(self.frame.size.width/2 + 25, self.frame.size.height/2 + 15);
     [self addChild:ball2];
-    
     ball2.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball2.frame.size.width/2];
     ball2.physicsBody.friction = 0.1f;
     ball2.physicsBody.restitution = 1.0f;
     ball2.physicsBody.linearDamping = 0.1f;
     ball2.physicsBody.allowsRotation = NO;
     
+    [ball2.physicsBody applyImpulse:CGVectorMake(20.0f, 5.0f)];
     
     SKSpriteNode* ball3 = [SKSpriteNode spriteNodeWithImageNamed: @"ball_red.png"];
     ball3.name = @"ball3";
     ball3.position = CGPointMake(self.frame.size.width/2, self.frame.size.height/2);
     [self addChild:ball3];
-    
     ball3.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball3.frame.size.width/2];
     ball3.physicsBody.friction = 0.1f;
     ball3.physicsBody.restitution = 1.0f;
@@ -112,7 +139,6 @@
     ball4.name = @"ball4";
     ball4.position = CGPointMake(self.frame.size.width/2 + 25, self.frame.size.height/2 - 15);
     [self addChild:ball4];
-    
     ball4.physicsBody = [SKPhysicsBody bodyWithCircleOfRadius:ball4.frame.size.width/2];
     ball4.physicsBody.friction = 0.1f;
     ball4.physicsBody.restitution = 1.0f;
@@ -120,18 +146,31 @@
     ball4.physicsBody.allowsRotation = NO;
     
     
-    SKShapeNode *cue = [SKShapeNode shapeNodeWithRect:CGRectMake(-80/2, -3, 80, 6)];
+    SKSpriteNode *cue = [SKSpriteNode spriteNodeWithImageNamed: @"Cue.png"];
     cue.name = @"Cue";
-    cue.position = CGPointMake(20, 160);
-    cue.strokeColor = [SKColor redColor];
-    cue.fillColor = [SKColor greenColor];
-    [self addChild:cue];
-    cue.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:cue.frame.size];
+    cue.position = CGPointMake(10, self.frame.size.height/2);
     cue.physicsBody.restitution = 0.9f;
     cue.physicsBody.friction = 0.4f;
-    // make physicsBody static
-    cue.physicsBody.dynamic = NO;
- 
+   
+    SKPhysicsBody *cue2 = [SKPhysicsBody bodyWithRectangleOfSize:cue.frame.size];
+    [cue setPhysicsBody:cue2];
+    
+    [cue2 setDynamic:NO];
+    cue2.categoryBitMask = cueCategory;
+    cue2.collisionBitMask = ballCategory;
+    cue2.contactTestBitMask = ballCategory;
+    
+    [cue runAction:[SKAction repeatActionForever:[SKAction rotateByAngle: (-2*M_PI) duration:10]]];
+    
+    [self addChild:cue];
+    //cue.physicsBody = [SKPhysicsBody bodyWithRectangleOfSize:cue.frame.size];
+    
+    //cue.physicsBody.dynamic = NO;
+    //SKPhysicsJointSpring *cueBall = [SKPhysicsJointSpring jointWithBodyA:cue2 bodyB:ball1 anchorA:CGPointMake(10, self.frame.size.height/2) anchorB:CGPointMake(self.frame.size.width/3, self.frame.size.height/2)];
+    //SKPhysicsJointPin *cueBall2 = [SKPhysicsJointPin jointWithBodyA:cue2 bodyB:ball1 anchor:CGPointMake(self.frame.size.width/3, self.frame.size.height/2)];
+    //SKPhysicsJointLimit *cueBall3 = [SKPhysicsJointLimit jointWithBodyA:cue2 bodyB:ball1 anchorA:CGPointMake(10, self.frame.size.height/2) anchorB:CGPointMake(self.frame.size.width/3, self.frame.size.height/2)];
+    //[self.physicsWorld addJoint:cueBall2];
+    
 }
 
 -(void)update:(CFTimeInterval)currentTime {
--- Pool/Pool/GameViewController.m
+++ Pool/Pool/GameViewController.m
@@ -11,20 +11,20 @@
 
 @implementation SKScene (Unarchive)
 
-+ (instancetype)unarchiveFromFile:(NSString *)file {
-    /* Retrieve scene file path from the application bundle */
-    NSString *nodePath = [[NSBundle mainBundle] pathForResource:file ofType:@"sks"];
-    /* Unarchive the file to an SKScene object */
-    NSData *data = [NSData dataWithContentsOfFile:nodePath
-                                          options:NSDataReadingMappedIfSafe
-                                            error:nil];
-    NSKeyedUnarchiver *arch = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
-    [arch setClass:self forClassName:@"SKScene"];
-    SKScene *scene = [arch decodeObjectForKey:NSKeyedArchiveRootObjectKey];
-    [arch finishDecoding];
-    
-    return scene;
-}
+//+ (instancetype)unarchiveFromFile:(NSString *)file {
+//    /* Retrieve scene file path from the application bundle */
+//    NSString *nodePath = [[NSBundle mainBundle] pathForResource:file ofType:@"sks"];
+//    /* Unarchive the file to an SKScene object */
+//    NSData *data = [NSData dataWithContentsOfFile:nodePath
+//                                          options:NSDataReadingMappedIfSafe
+//                                            error:nil];
+//    NSKeyedUnarchiver *arch = [[NSKeyedUnarchiver alloc] initForReadingWithData:data];
+//    [arch setClass:self forClassName:@"SKScene"];
+//    SKScene *scene = [arch decodeObjectForKey:NSKeyedArchiveRootObjectKey];
+//    [arch finishDecoding];
+//    
+//    return scene;
+//}
 
 @end
 
@@ -32,22 +32,26 @@
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
+   
+}
 
+-(void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+    
     // Configure the view.
     SKView * skView = (SKView *)self.view;
-    skView.showsFPS = YES;
-    skView.showsNodeCount = YES;
-    /* Sprite Kit applies additional optimizations to improve rendering performance */
-    skView.ignoresSiblingOrder = YES;
-    skView.showsPhysics = NO;
-    
-    // Create and configure the scene.
-    GameScene *scene = [GameScene unarchiveFromFile:@"GameScene"];
-    scene.scaleMode = SKSceneScaleModeResizeFill;
-    
-    // Present the scene.
-    [skView presentScene:scene];
+    if (!skView.scene) {
+        skView.showsFPS = YES;
+        skView.showsNodeCount = YES;
+        //skView.showsPhysics = YES;
+        // Create and configure the scene.
+        SKScene * scene = [GameScene sceneWithSize:skView.bounds.size];
+        scene.scaleMode = SKSceneScaleModeAspectFill;
+        //scene.anchorPoint = CGPointMake(0, 320);
+        
+        // Present the scene.
+        [skView presentScene:scene];
+    }
 }
 
 - (BOOL)shouldAutorotate
